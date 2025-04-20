732f7dbb3e134e21c8a78c6f1da153f2bbf037b3
SarakshiKaur <sarakshi2003@gmail.com>
2025-04-20 20:18:45 +0530

    Initialized with go module and defined packages

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
-	"fmt"
+    "fmt" // for printing or returning formated strings
+    "log"  // for sending error logs
+    "encoding/json"  // for encoding and decoding things to json
+    "math/rand"   // for generating random numbers
+    "net/http"  // for creating http servers and routes
+    "strconv"  // for converting strings
+    "github.com/gorilla/mux"
 )
 
 func main() {
